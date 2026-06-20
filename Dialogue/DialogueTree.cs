19f5222edaa4300e671d3f93dce144846c3a99b7
Miriam Vtk <witekmiriam@gmail.com>
2026-06-21 00:42:43 +0200

    cc

---
+++ Dialogue/DialogueTree.cs
@@ -0,0 +1,22 @@
+﻿namespace HauntedMansion.Dialogue
+{
+    /// <summary>
+    /// holds a complete dialogue tree from json
+    /// </summary>
+    public class DialogueTree
+    {
+        private readonly Dictionary<string, DialogueNode> _nodes = new();
+
+        public void AddNode(DialogueNode node)
+        {
+            _nodes[node.NodeID] = node;
+        }
+        /// <summary>
+        /// returns node by ID (or null)
+        /// </summary>
+        public DialogueNode GetNode(string nodeId)
+        {
+            return _nodes.TryGetValue(nodeId, out var node) ? node : null;
+        }
+    }
+}

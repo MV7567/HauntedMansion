7d46e949073da692fc58613a5d289f17f0053d99
Miriam Vtk <witekmiriam@gmail.com>
2026-06-22 16:45:26 +0200

    changes

---
+++ UI/ConsoleInputProvider.cs
@@ -0,0 +1,21 @@
+﻿namespace HauntedMansion.UI
+{
+    public class ConsoleInputProvider : IInputProvider
+    {
+        public int GetIntInput(int min, int max)
+        {
+            while (true)
+            {
+                if (int.TryParse(Console.ReadLine(), out int choice) && choice >= min && choice <= max)
+                    return choice;
+                    
+                Console.Write($"Invalid choice (must be {min}-{max}). Try again: ");
+            }
+        }
+
+        public void WaitForContinue()
+        {
+            Console.ReadLine();
+        }
+    }
+}
